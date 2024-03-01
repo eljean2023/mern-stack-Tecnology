d77d6e9d3fa3ff58bb06b22c6dedaf7a6d68160d
Bawoolx <bawoolcontact@gmail.com>
2024-02-29 20:31:04 -0600

    V1.0.0.11

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,9 @@ mongoose.set('strictQuery', true);
 
 /*const URI_CON ='mongodb://127.0.0.1:27017/jwtData1'; */
 
-const URI_CON ='mongodb://127.0.0.1:27017/justStars';
+/*const URI_CON ='mongodb://127.0.0.1:27017/justStars';*/
+
+const URI_CON ='mongodb+srv://eljean2024:mern-technology.user2024!@cluster0.ttxex.mongodb.net/mern-technology.user?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(URI_CON)
 
