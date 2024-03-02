ad870213a8f90fe6599a50ef6139e26be0b7715f
Bawoolx <bawoolcontact@gmail.com>
2024-03-02 02:07:50 -0600

    V1.0.0.15

--- backend/database.js
+++ backend/database.js
@@ -6,9 +6,11 @@ mongoose.set('strictQuery', true);
 
 /*const MONGODB_URI ='mongodb://127.0.0.1:27017/justStars'; */
 
- const MONGODB_URI ="mongodb+srv://eljean2024:mern-technology.user2024!@cluster0.ttxex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+/* const MONGODB_URI ="mongodb+srv://eljean2024:mern-technology.user2024!@cluster0.ttxex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"*/
 
 
+const MONGODB_URI =  process.env.MONGODB_URI
+console.log(" hellow" + MONGODB_URI)
 mongoose.connect(MONGODB_URI)
 
 .then(db =>console.log('mongo database is running', db.connection.host))
