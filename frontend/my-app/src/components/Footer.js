4ba33f43f41d6ebea8a1a733fb14f24cdea57235
Bawoolx <bawoolcontact@gmail.com>
2024-03-01 19:46:59 -0600

    V1.0.0.13

--- frontend/my-app/src/components/Footer.js
+++ frontend/my-app/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   <footer>
         <Container>
         <Row>
-        <Col id='footer' className='text-center py3 animate__animated animate__fadeInLeft animate__slower animate__delay-2s	4s fixed-bottom'>Copyright &copy; Bawool industry support 2023 </Col>
+        <Col id='footer' className='text-center py3 animate__animated animate__fadeInLeft animate__slower animate__delay-2s	4s fixed-bottom'>Copyright &copy; Bawool industry support 2024 </Col>
         </Row>
         </Container>
   </footer>    
